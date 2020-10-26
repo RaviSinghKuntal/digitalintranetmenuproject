395aa7e736becc14eb5f07033998f88d1f2734e3
Vivek <vivek.vikram@daffodilsw.com>
2020-10-26 12:33:06 +0530

    bootstrap

--- client/src/app/modules/manageFood/items/items.component.ts
+++ client/src/app/modules/manageFood/items/items.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-items',
